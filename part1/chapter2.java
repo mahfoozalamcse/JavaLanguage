3badf153bacbd0f87e2f60255f055574c1519552
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-03-19 19:02:27 +0530

    Update chapter2.java

--- part1/chapter2.java
+++ part1/chapter2.java
@@ -12,7 +12,7 @@ public class chapter2 {
      System.out.println("Enter second number :");
      int num2 = sc.nextInt();
      System.out.println("Enter third number :");
-     int num3 = sc.nextInt();
+     int num3 = sc.nextInt(); 
 
      int greater = (num1 > num2) ?(num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
      System.out.println(greater);
