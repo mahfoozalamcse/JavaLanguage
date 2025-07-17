6265d34b337a366efef2dac24cd8907b34021545
mahfoozalamcse <amahfooz1007@gmail.com>
2025-07-18 01:54:03 +0530

    practice basic java syntex

---
+++ Basic1/basic.java
@@ -0,0 +1,33 @@
+package Basic1;
+
+public class basic {
+    // write first program 
+    public static void main(String[] args) {
+        String name = "Mahfooz Alam";
+        System.out.println(name);
+
+        // variable in java
+        int age = 36;
+        String name2 = "AG";
+        double b = 40.5;
+        System.out.println(age);
+        System.out.println(b);
+        System.out.println(name2);
+
+        // variable naming
+        /*
+         * ab , $ab, cABB
+         * // but exact name understandable good name
+         * eg. name, age, className etc..
+         * 
+         * not valid => 11ab , a#, a!, else, if ete .. 
+         */
+
+         /*
+          * Type of variable
+           1. primitive int char float double boolean etc
+           2. Non primitive  string arr and any user defined class
+          */
+
+    }
+}
