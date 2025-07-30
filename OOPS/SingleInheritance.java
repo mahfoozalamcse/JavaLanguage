7cd3d5a36bc0f2a5fae529f9b80a87f2a46790cf
mahfoozalamcse <amahfooz1007@gmail.com>
2025-07-30 21:57:58 +0530

    oops practice

--- OOPS/SingleInheritance.java
+++ OOPS/SingleInheritance.java
@@ -1,4 +1,5 @@
 package OOPS;
+// interface like - comparable, runnable,printable like name exist 
 
 public class SingleInheritance {
     void show(){
@@ -17,5 +18,30 @@ class B{
         child ch = new child();
         ch.display();
         ch.show();
+
+        // test default case
+        Test i = new Test();
+        i.fun1();
+        i.fun2();
+        i.fun3();
     }
+}
+
+// more default example of interface
+interface MyInt{
+    default void fun1(){
+        System.out.println("fun1");
+    }
+    default void fun2(){
+        System.out.println("fun2");
+    }
+
+    void fun3();
+}
+
+class Test implements MyInt{
+    public void fun3(){
+        System.out.println(" Fun 3");
+    }
+
 }
