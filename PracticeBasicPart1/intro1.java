07dfe0a4964b0be5f2bfeb28a2dd824a76578c74
mahfoozalamcse <amahfooz1007@gmail.com>
2025-10-07 20:02:20 +0530

    practice intro in java

---
+++ PracticeBasicPart1/intro1.java
@@ -0,0 +1,60 @@
+package PracticeBasicPart1;
+
+public class intro1 {
+    // Why do we need Programming language?
+    // 1. Compilers
+    // 2. Interpreters
+    // Introduction to java
+    // 1. Platform Independent
+    // How Java Works
+    // What is Java Virtual Machine (JVM)?
+    // Similarities and Differences of Java with C/C++ and Python
+    
+
+    // Advantages of Java Over C++
+    // 1. Automatic Garbage Collection
+    // 2. Better Exception Handling
+    // 3. Enhanced Multithreading
+    // 4. Difficult to Write Bad Code
+
+
+    // Writing First Program in Java
+    // Explain JDK and JRE
+
+
+    // how JVM work
+    // Interpreter
+    // JIT (Just-In-Time Compilation)
+    
+
+    // Developer Tools in JDK
+    /*
+     * Java Compiler: Converts Java source code into bytecode.
+     * Java Debugger: Helps identify and fix issues in your code.
+     * Java Document Generator: Creates documentation from your code comments.
+     */
+
+
+     public static void main(String[] args) {
+        System.out.println("Hello , JAVA!");
+     }
+
+     // 1. Class Defination
+     /*
+      * Main Method
+      * The main method is the entry point for the program
+      */
+
+      // public static void main(String[] args)
+      /*
+       * public: So that JVM can execute the method from anywhere.
+       * static: Main method is to be called without object. The modifiers public and
+       * static can be written in either order.
+       * void: The main method doesn't return anything.
+       * main(): Name configured in the JVM.
+       * String[]: The main method accepts a single argument: an array of elements of
+       * type String.
+       */
+
+       
+}
