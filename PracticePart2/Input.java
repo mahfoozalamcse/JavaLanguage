142de92d3ca766899c1d5d4e5782723908b7760c
mahfoozalamcse <amahfooz1007@gmail.com>
2025-10-08 01:51:27 +0530

    take input from user

---
+++ PracticePart2/Input.java
@@ -0,0 +1,25 @@
+package PracticePart2;
+
+import java.util.Scanner;
+
+public class Input {
+
+    public static void main(String[] args) {
+
+      Scanner sc = new Scanner(System.in);
+
+      String name = sc.nextLine();
+      int age = sc.nextInt();
+      long number = sc.nextLong();
+      char gender = sc.next().charAt(0);
+
+      System.out.println("User name "+ name);
+      System.out.println("User Age "+ age);
+      System.out.println("User number "+ number);
+      System.out.println("User gender "+ gender);
+
+
+      sc.close();
+
+    }
+}
