f7720914475ad75bc86cf8b0f8b1f74ac6d589bf
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-11-23 20:44:32 +0530

    Add elements to ArrayList in main method

--- ArrayList/ArrayListTraversal.java
+++ ArrayList/ArrayListTraversal.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class ArrayListTraversal {
     public static void main(String[] args) {
         ArrayList<Integer> al = new ArrayList<>();
+        
         al.add(10);
         al.add(20);
         al.add(30);
