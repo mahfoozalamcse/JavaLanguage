5695444ac065e01ecdf18901a32367166fc3a2be
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2026-03-12 10:20:17 +0530

    Remove empty line in ArrayIntro.java

--- ArrayList/ArrayIntro.java
+++ ArrayList/ArrayIntro.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class ArrayIntro {
     public static void main(String[] args) {
         
-        
         ArrayList<Integer> arr = new ArrayList<>();
         arr.add(10);
         arr.add(20);
