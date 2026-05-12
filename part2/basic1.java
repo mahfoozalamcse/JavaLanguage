d561c94815d91124f9e274be1a3d96928c3d3bd5
mahfoozalamcse <amahfooz1007@gmail.com>
2026-05-12 22:42:21 +0530

    know basic of system information

---
+++ part2/basic1.java
@@ -0,0 +1,11 @@
+package part2;
+
+public class basic1 {
+    
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("java.version"));
+        System.out.println(System.getProperty("user.name"));
+    }
+}
+
